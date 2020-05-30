35746e6aed20bd5dd4b6bc35bd6d3655df925ec4
Arthur Egide <arthuregide@gmail.com>
2020-05-30 01:31:50 -0300

    Saldo diário

--- src/main/java/estagiario/WorkDay.java
+++ src/main/java/estagiario/WorkDay.java
@@ -1,15 +1,42 @@
 package estagiario;
 
-import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class WorkDay {
-  String in, out, day_balance;
+  Date in, out;
   String note;
 
   WorkDay(String i, String o, String n){
-    this.in = i;
-    this.out = o;
+    this.in  = stringToDate(i);
+    this.out = stringToDate(o);
     this.note = n;
+  }
+
+  Date stringToDate(String d){
+    try{
+
+      SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+      Date date = (Date)formatter.parse(d);
+      return date;
+    }catch(Exception e){
+      e.printStackTrace();
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Escreva a data desejada com a seguinte formatacao:  ");
+      System.out.println("=-=-=- HH:mm:ss");
+      System.out.println("=EX:=- 09:01:00\n");
+      System.out.println("");
+      String data = sc.nextLine();
+      sc.close();
+      return stringToDate(data);
+    }
+  }
+
+  double getBalance(){
+    long secs = ((this.out.getTime() - this.in.getTime()) / 1000) > 0 ? (this.out.getTime() - this.in.getTime()) / 1000 : (this.in.getTime() - this.out.getTime()) / 1000;
+    double mins = secs / 60.00;
+    return mins;
   }
 
   @Override
